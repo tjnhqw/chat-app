cdccc15ad20158a724103c313436a59314ef4c28
tjnhqw <tjnhqw@gmail.com>
2024-06-14 15:06:45 +0700

    rm app page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className='flex h-screen w-screen justify-center items-center'>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 }
