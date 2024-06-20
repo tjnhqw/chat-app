756c04a57bdacdc51adcf1b525f7d95768a1049e
tjnhqw <tjnhqw@gmail.com>
2024-06-20 13:24:39 +0700

    isSignedIn

--- components/sidebar/SidebarWrapper.tsx
+++ components/sidebar/SidebarWrapper.tsx
@@ -3,14 +3,24 @@ import React from 'react'
 import DesktopNav from './nav/DesktopNav'
 import MobileNav from './nav/MobileNav'
 import { useIsMobile } from '~/hooks/useIsMobile'
+import { useAuth } from '@clerk/nextjs'
 
 type Props = React.PropsWithChildren<{}>
 
 const SidebarWrapper = ({ children }: Props) => {
+  const { isSignedIn } = useAuth();
   const isMobile = useIsMobile();
   return (
     <div className='h-full w-full p-4 flex flex-col lg:flex-row gap-4'>
-      {isMobile ? <MobileNav /> : <DesktopNav />}
+      <>
+        {
+          isSignedIn &&
+          <>
+            {isMobile ? <MobileNav /> : <DesktopNav />}
+          </>
+        }
+      </>
+
       <main className='h-[calc(100%-80px)] lg:h-full w-full block gap-4 lg:flex relative'>
         {children}
       </main>
